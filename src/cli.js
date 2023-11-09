184e8fa26e5d1ceb25ee1f6c3b0df7d3adba03a7
Erokhin <8batos@gmail.com>
2023-11-09 12:09:10 +0300

    step 3

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from '../readline-sync/lib/readline-sync.js';
+const greeting = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + name + '!')
+}
+
+export default greeting;
